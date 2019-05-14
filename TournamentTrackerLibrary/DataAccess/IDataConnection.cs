549c26b5faa9e17a19d8149c8e6f2307aebada3d
Shane D Wilson <me@shanedwilson.com>
2019-05-14 13:47:43 -0500

    organized files in folders

--- TournamentTrackerLibrary/DataAccess/IDataConnection.cs
+++ TournamentTrackerLibrary/DataAccess/IDataConnection.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using TrackerLibrary;
+using TrackerLibrary.Models;
 
-namespace TournamentTrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
     public interface IDataConnection
     {
